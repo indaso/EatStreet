3ae02867ec25bdc819242bf135db33a0d2f3b0e4
indaso <indaso5@gmail.com>
2015-01-17 13:16:06 -0500

    working on geolocation

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+	res.render('index', {
+		title: 'Express'
+	});
 });
 
-router.post('/createGroup', function(req, res) {
+router.post('/createGroup', function (req, res) {
 	var db = req.db;
 
 	var name = req.body.name;
@@ -14,9 +16,14 @@ router.post('/createGroup', function(req, res) {
 
 	var group = db.get('group');
 
-	var newuser = {"_id":groupId,"members":[{"name":name}]};
+	var newuser = {
+		"_id": groupId,
+		"members": [{
+			"name": name
+		}]
+	};
 
-	group.insert(newuser,function(err, doc) {
+	group.insert(newuser, function (err, doc) {
 		if (err)
 			res.send("Error: Unable to add information to the database");
 		else {
@@ -25,7 +32,7 @@ router.post('/createGroup', function(req, res) {
 	})
 });
 
-routes.get('/map',function(req,res) {
-	
+router.get('/map', function (req, res) {
+
 });
 module.exports = router;

9fa9523ea9d5151af9f35d47c11d09b57a1d0051
Stephanie Hsu <stephanie.hsu888@gmail.com>
2015-01-17 12:35:17 -0500

    set up mongo, can insert into db

--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,45 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/createGroup', function(req, res) {
+
+router.post('/', function(req, res) {
 	var db = req.db;
+
+	console.log(db);
 
 	var name = req.body.name;
 	var groupId = Math.floor(Math.random() * 10000);
 
-	var group = db.get('group');
+	var group = db.get('users');
 
-	var newuser = {"_id":groupId,"members":[{"name":name}]};
-
-	group.insert(newuser,function(err, doc) {
-		if (err)
-			res.send("Error: Unable to add information to the database");
-		else {
-			res.redirect("map");
-		}
-	})
-});
-
-routes.get('/map',function(req,res) {
 	
+	var latitude, longitude;
+
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition(function showPosition(position) {
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
+		});
+	}
+	else {
+		console.log("ERROR");
+	}
+
+
+	var new_group = {"groupid":groupId,"members":[{"name":name}]};
+	group.insert(new_group,function(err, doc) {
+			if (err)
+				res.send("Error: Unable to add information to the database");
+			else {
+				console.log("ADDED");
+				res.redirect("map");
+			}
+		});
 });
+
+router.get('/map',function(req,res) {
+	res.render('index', { title: 'MAP' });
+});
+
+
 module.exports = router;

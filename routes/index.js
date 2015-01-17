46d56cdc2da5f46b93efe19a1e51d8451895f81a
Stephanie Hsu <stephanie.hsu888@gmail.com>
2015-01-17 15:50:02 -0500

    calculates center

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -18,9 +19,8 @@ router.post('/', function (req, res) {
 	var groupId = req.body.groupid;
 	var latitude = req.body.latitude;
 	var longitude = req.body.longitude;
-	
+
 	if (name != "" && groupName != "") {	
-		console.log("HERE");
 		var new_group = {
 			"groupid": groupName,
 			"members": [{"name": name, "latitude": latitude, "longitude":longitude}]
@@ -29,13 +29,14 @@ router.post('/', function (req, res) {
 			if (err)
 				res.send("Error: Unable to add information to the database");
 			else {
+				req.session.group = groupName;
 				res.redirect("map");
 			}
 		});
+
 	}
 
 	else if (username != "" && groupId != "") {
-		console.log("JOIN GROUP");
 		var currGroup = users.find({"groupid":groupId});
 		if (currGroup == "") {
 			console.log("Error: Unable to find group in the database");
@@ -46,6 +47,7 @@ router.post('/', function (req, res) {
 				if (err)
 					res.send("Error: Unable to add information to the database");
 				else {
+					req.session.group = groupId;
 					res.redirect("map");
 				}
 			});
@@ -57,9 +59,44 @@ router.post('/', function (req, res) {
 });
 
 router.get('/map', function (req, res) {
-	res.render('index', {
-		title: 'MAP'
+	var db = req.db;
+	var users = db.get('users');
+	var groupid = req.session.group;
+	console.log("GROUP ID: " + groupid);
+	var sumX = 0;
+	var sumY = 0;
+	var centerX;
+	var centerY;
+	var all_coords = [];
+
+	users.findOne({"groupid":groupid}, function(err, document) {
+		if (err)
+			throw err;
+		else {
+			var members = document.members;
+			for (var i = 0; i < members.length; i++	) {
+				var coord = [parseFloat(members[i].latitude),parseFloat(members[i].longitude)];
+				all_coords.push(coord);
+				sumX += parseFloat(members[i].latitude);
+				sumY += parseFloat(members[i].longitude);
+			}
+			centerX = sumX / members.length;
+			centerY = sumY / members.length;
+			var center = [centerX, centerY];
+
+			console.log("Center X : " + centerX);
+			console.log("Center Y : " + centerY);
+			console.log(all_coords);
+
+			res.render('index', {
+				title: 'MAP',
+				coords: all_coords,
+				center: center
+			});
+		}
 	});
+	
+	
 });
 
 

61ce5ddd121b0dfb283d9c0b00765985187ce89e
Stephanie Hsu <stephanie.hsu888@gmail.com>
2015-01-17 00:54:26 -0500

    first functionality

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/createGroup', function(req, res) {
+	var db = req.db;
+
+	var name = req.body.name;
+	var groupId = Math.floor(Math.random() * 10000);
+
+	var group = db.get('group');
+
+	var newuser = {"_id":groupId,"members":[{"name":name}]};
+
+	group.insert(newuser,function(err, doc) {
+		if (err)
+			res.send("Error: Unable to add information to the database");
+		else {
+			res.redirect("map");
+		}
+	})
+});
+
+routes.get('/map',function(req,res) {
+	
+});
 module.exports = router;
